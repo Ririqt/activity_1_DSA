b4a593c1ecd7c591137a66793a357c8905d4b21d
Rei Rigos <mcrei1018@gmail.com>
2024-09-25 09:54:22 +0800

    Creating Condition for Celsius to Fahrenheit

--- exercise_1_temperature_conversion.py
+++ exercise_1_temperature_conversion.py
@@ -2,3 +2,5 @@ while True:
     try:
         temperature = float(input("What is your Temperature? "))
         conversion = int(input("Type '1' for (Celsius to Fahrenheit), Type '2' for (Fahrenheit to Celsius): "))
+        if conversion == 1:
+            
