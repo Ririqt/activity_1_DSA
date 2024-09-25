6802a667bcd8d2bdcb3cea3e0caf2ace04f48082
Rei Rigos <mcrei1018@gmail.com>
2024-09-25 11:18:10 +0800

    Putting the Formula of Current

--- exercise_2_ohm's law_calculator.py
+++ exercise_2_ohm's law_calculator.py
@@ -2,5 +2,6 @@ def voltage_formula(current, resistance):
      return current * resistance
 
 def current_formula(voltage, resistance):
-     if resistance == 0:
+    if resistance == 0:
         raise ZeroDivisionError("Sorry, Resistance cannot be Zero.")
+    return voltage / resistance
