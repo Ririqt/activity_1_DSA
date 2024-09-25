0019f3b0858dc25fd621e66fa7af206eec694ce2
Rei Rigos <mcrei1018@gmail.com>
2024-09-25 09:51:49 +0800

    Asking what to convert

--- exercise_1_temperature_conversion.py
+++ exercise_1_temperature_conversion.py
@@ -1,4 +1,4 @@
 while True:
     try:
         temperature = float(input("What is your Temperature? "))
-        
+        conversion = int(input("Type '1' for (Celsius to Fahrenheit), Type '2' for (Fahrenheit to Celsius): "))
